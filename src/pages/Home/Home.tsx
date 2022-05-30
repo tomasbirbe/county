f66493e387b1a7266fd4b653209f3470358ea1fc
Tomas Birbe <tomas.birbe@gmail.com>
2022-05-29 23:31:21 -0300

    Dev: Add some basic logic to calculate overviews on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Container, Img, Stack, Text } from "@chakra-ui/react";
 
 import ArrowUpIcon from "/Icons/arrow-up.svg";
 import SavingsIcon from "/Icons/savings.svg";
 import ArrowDownIcon from "/Icons/arrow-down.svg";
 
+import moneyFormatter from "src/utils/moneyFormatter";
+
 export const Home: React.FC = () => {
+  function totalSpends() {
+    const spends = JSON.parse(localStorage.getItem("spends") || "[]");
+
+    return spends.reduce((acc, spend) => acc + Number(spend.amount), 0);
+  }
+
+  function totalSavings() {
+    const savings = JSON.parse(localStorage.getItem("savings") || "[]");
+
+    return savings.reduce((acc, saving) => acc + Number(saving.amount), 0);
+  }
+
+  function totalIncomes() {
+    const incomes = JSON.parse(localStorage.getItem("incomes") || "[]");
+
+    return incomes.reduce((acc, income) => acc + Number(income.amount), 0);
+  }
+
   return (
     <Container as="main" height="calc(100% - 70px)" maxWidth="full" paddingX={0}>
       <Stack as="header" height="full">
@@ -13,7 +33,7 @@ export const Home: React.FC = () => {
           <Text variant="h3">Gastos</Text>
           <Stack align="center" direction="row">
             <Img height="55px" src={ArrowUpIcon} width="55px" />
-            <Text variant="h1">$50000</Text>
+            <Text variant="h1">{moneyFormatter(totalSpends())}</Text>
           </Stack>
         </Stack>
 
@@ -22,7 +42,7 @@ export const Home: React.FC = () => {
             <Text variant="h4">Ahorros</Text>
             <Stack align="center" direction="row">
               <Img height="45px" src={SavingsIcon} width="45px" />
-              <Text variant="h2">$50000</Text>
+              <Text variant="h2">{moneyFormatter(totalSavings())}</Text>
             </Stack>
           </Stack>
           <Box bg="blackAlpha.300" height="60%" width="1px" />
@@ -30,7 +50,7 @@ export const Home: React.FC = () => {
             <Text variant="h4">Ingresos</Text>
             <Stack align="center" direction="row">
               <Img height="45px" src={ArrowDownIcon} width="45px" />
-              <Text variant="h2">$50000</Text>
+              <Text variant="h2">{moneyFormatter(totalIncomes())}</Text>
             </Stack>
           </Stack>
         </Stack>
