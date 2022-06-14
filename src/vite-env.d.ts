a13cda57c83b4dd029039a5756c320f7e7261649
Tomas Birbe <tomas.birbe@gmail.com>
2022-06-14 18:37:33 -0300

    Dev: Add env file with apikey for firebase

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+import.meta.env.firebaseConfig = {
+  apiKey: "AIzaSyB4dKLVoLvCOcxNYSJ7Qi1eAg90V4Y6Q5k",
+  authDomain: "county-944fb.firebaseapp.com",
+  projectId: "county-944fb",
+  storageBucket: "county-944fb.appspot.com",
+  messagingSenderId: "222442525133",
+  appId: "1:222442525133:web:4604793651ae0e30547484",
+};
