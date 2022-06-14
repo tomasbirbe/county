899d173812fbf60e632b1510a487df0441cdf200
Tomas Birbe <tomas.birbe@gmail.com>
2022-06-14 18:12:26 -0300

    Dev: Add config of firebase to gitignore

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB4dKLVoLvCOcxNYSJ7Qi1eAg90V4Y6Q5k",
-  authDomain: "county-944fb.firebaseapp.com",
-  projectId: "county-944fb",
-  storageBucket: "county-944fb.appspot.com",
-  messagingSenderId: "222442525133",
-  appId: "1:222442525133:web:4604793651ae0e30547484",
-};
+import firebaseConfig from "src/firebaseConfig";
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
